15e8630c47ee1ff09baa5ca6689a21dba775a3bb
Ali Rizvi <frontear4594@gmail.com>
2021-10-12 21:01:01 -0400

    add basic pygame code

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-def main():
-    print("Hello, World!")
+import pygame
+from pygame.locals import *
+
+def tick(surface: pygame.Surface) -> bool:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            return False
+
+    surface.fill((255, 255, 255))
+    
+    return True
+
+def main() -> None:
+    pygame.init()
+    
+    SURFACE = pygame.display.set_mode((640, 360))
+    CLOCK = pygame.time.Clock()
+
+    while tick(SURFACE):
+        pygame.display.update()
+        CLOCK.tick(60)
+    
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
