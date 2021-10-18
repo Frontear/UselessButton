37f8ed549dce5f1f05fa592f5a651653cfe2a08e
Ali Rizvi <frontear4594@gmail.com>
2021-10-17 23:51:08 -0400

    create base class again
    I want to abstract.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,32 @@
-def main():
-    print("Hello, World!")
+import pygame
+import logging
+
+from pygame.locals import *
+
+def main(logger: logging.Logger):
+    _, fails = pygame.init()
+    if fails > 0:
+        logger.warning(f"some pygame modules failed to init ({fails} failed)")
+    
+    pygame.display.set_mode((640, 360))
+    pygame.display.set_caption("Useless Button")
+    pygame.display.get_surface().fill((200, 200, 200))
+
+    ticker = pygame.time.Clock()
+
+    run = True
+    while run:
+        ticker.tick(60)
+
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                run = False
+        
+        pygame.display.update()
+
+    pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(format='[%(asctime)s] [%(name)s/%(levelname)s]: %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+    main(logging.getLogger("UselessButton"))
