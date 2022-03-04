d1a7841cadb85f8a4ee2334ae60ba7e208f7ed8a
Ali RIzvi <frontear4594@gmail.com>
2022-03-04 12:05:52 -0500

    rewrite everything
    more displeased with the rewrite than the original honestly. i hate the unnecessary abstractions cutting into performance.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,2 @@
-from button import Button
-import window
-import logging
-
-from pygame.locals import *
-
-def main():
-    logging.basicConfig(format='[%(asctime)s] [%(name)s/%(levelname)s]: %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
-    screen = window.Window(logging.getLogger("UselessButton"))
-
-    screen.create_display("UselessButton", 640, 360, 60)
-    screen.add_renderable(Button())
-    screen.run()
-    screen.close()
-
 if __name__ == "__main__":
-    main()
+    print("Hello, World!")
